5713279e72b1ecd2763544428c48b69babbcaad3
翁心苹 <ohmyhappy@oukokoropingdeMacBook-Air.local>
2016-01-18 20:25:54 +0800

    2016/01/18＿專案建立

---
+++ MonsterMap/MonsterMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MonsterMap
+//
+//  Created by 翁心苹 on 2016/1/15.
+//  Copyright © 2016年 MonsterTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MonsterMap/MonsterMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MonsterMap
+//
+//  Created by 翁心苹 on 2016/1/15.
+//  Copyright © 2016年 MonsterTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
